47dc145edc3ea94ebd8ab2f3775258ef48a3132e
ayman <antoinengn45@gmail.com>
2026-05-28 20:36:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ function checkLogin() {
     const loginBox = document.getElementById('login-box');
     const profile = document.getElementById('profile-section');
 
-    // Correction : Ajout de l'accolade ouvrante ici
     if (p === 'ayman' || p === 'aygame') {
         loginBox.style.display = 'none';
         profile.style.display = 'none';
@@ -17,6 +16,8 @@ function checkLogin() {
 function generateurSEO() {
     const titre = document.getElementById('titreVideo').value;
     const count = titre.length;
-    let feedback = count > 60 ? "Titre trop long." : (count < 20 ? "Titre trop court." : "Titre parfait !");
-    document.getElementById('seo-feedback').innerText = `${count}/100 caractères - ${feedback}`;
+    const feedback = document.getElementById('seo-feedback');
+    
+    let message = count > 60 ? "Titre trop long." : (count < 20 ? "Titre trop court." : "Titre parfait !");
+    feedback.innerText = `${count}/100 caractères - ${message}`;
 }
